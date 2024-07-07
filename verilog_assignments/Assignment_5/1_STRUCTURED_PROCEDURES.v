bea380d99fab71bbec3fe43fe3bb847edc99b4fd
mohamedsharith <mohdsharith@gmail.com>
2024-07-07 17:00:11 +0530

    Create 1_STRUCTURED_PROCEDURES.v

---
+++ verilog_assignments/Assignment_5/1_STRUCTURED_PROCEDURES.v
@@ -0,0 +1,14 @@
+1.How many DFFs will be inferred from the below snippet:
+input c, din;
+output reg y3;
+reg y1, y2;
+
+always@(posedge c) begin
+    y1 = din;
+    y2 = y1;
+    y3 = y2;
+end
+
+
+
+// answer: 1 d flipflop
