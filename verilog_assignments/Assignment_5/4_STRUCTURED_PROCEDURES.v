6d024291123ab033835d4576ad9aeb4bb36e4d09
mohamedsharith <mohdsharith@gmail.com>
2024-07-07 17:03:53 +0530

    Create 4_STRUCTURED_PROCEDURES.v

---
+++ verilog_assignments/Assignment_5/4_STRUCTURED_PROCEDURES.v
@@ -0,0 +1,15 @@
+4.Which of the statements will execute from the below snippet & justify the behavior?
+
+reg c = 3'b00x;
+
+always @(*) begin
+    case (c)
+        3'b000: st1;
+        3'b100: st2;
+        3'b001: st3;
+    endcase
+end
+
+
+
+// answer: st1 will execute.
